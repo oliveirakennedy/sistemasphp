5d043ade823b3bd07ce3d68216af56f3c557dc88
Wadson Garbes <wadson.goncalves@fatec.sp.gov.br>
2019-04-05 22:25:41 -0300

    adiciona index.php

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Skyfal Sistems</title>
+	<meta name="viewport" content=" e width=device-width, inicial-scale=1,0">
+	<meta name="keywords" content="Sistemas, web, barato">
+	<meta name="description" content="Sistemas web responsivos a preços acessíveis">
+	<meta charset="utf-8">
+</head>
+<body>
+	<header>
+		<div class="logo">Logomarca</div><!-- logo -->
+		<nav class="desktop">
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Sobre</a></li>
+				<li><a href="#">Serviços</a></li>
+				<li><a href="#">Contato</a></li>
+			</ul>
+		</nav>
+		<nav class="mobile">
+			<ul>				
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Sobre</a></li>
+				<li><a href="#">Serviços</a></li>
+				<li><a href="#">Contato</a></li>
+
+			</ul>		
+		</nav>
+	</header>
+
+	<section class="banner principal">
+		<form>
+			<h2>Qual o seu melhor e-mail?</h2>
+			<input type="email" name="email" required>
+			<input type="submit" name="acao" value="Cadastrar">
+		</form>
+		
+	</section><!-- banner-principal-->
+
+	<section class="descricao-autor">
+		<h2>Kennedy Oliveira</h2>
+		<div class="w50">
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus </p>
+		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus </p>
+		</div><!-- w50 -->
+
+		<div class="w50">
+			<!-- Pegar Imagem -->
+			<img src="">
+		</div><!-- w50 -->
+
+	</section><!--descrição autor -->
+
+
+</body>
+</html>
